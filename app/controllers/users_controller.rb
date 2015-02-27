036e23c8c7023d0bfa6abc71b05c6808b3b97ee8
Vedika Birla <birla22v@mtholyoke.edu>
2015-02-27 16:08:19 -0500

    Added models and made the views uglier

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @users = User.all
+    # @users = User.all
     if params[:query]
-      @query = RSpotify::Track.search(params[:query], limit: 20)
+      @query = RSpotify::Track.search(params[:query], limit: 10)
     end
   end
 
