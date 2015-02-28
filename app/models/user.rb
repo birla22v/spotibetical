387bfd74f027fa1c3de8ee497d9291dd99a8243f
Vedika Birla <birla22v@mtholyoke.edu>
2015-02-28 14:15:45 -0500

    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :votes
+  has_many :songs, through: :votes
+
+
 
    def vote
    end
