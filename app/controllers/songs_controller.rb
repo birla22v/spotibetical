d6363afb78b50523b93dcaadd18c08275189d061
Vedika Birla <birla22v@mtholyoke.edu>
2015-03-01 09:16:38 -0500

    Showing songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,18 @@
 class SongsController < ApplicationController
+
   def show
     @songs = Song.all
+    render 'users/index'
   end
 
-  def submit(song)
-    @song = Song.create(song)
-    # render :show
+  def submit
+    @song = Song.create(song_params)
+     show
   end
+  private
+
+  def song_params
+    params.require(:song).permit(:user_id, :name, :artist)
+  end
+
 end
