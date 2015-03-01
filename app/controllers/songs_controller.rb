ea204c1b87c4806be3bd08ae385edbf2fa243fd2
Vedika Birla <birla22v@mtholyoke.edu>
2015-03-01 11:28:41 -0500

    Songs are unique to playlist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,11 +6,23 @@ class SongsController < ApplicationController
   end
 
   def submit
-    @song = Song.create(song_params)
-     show
+    isthere = 0
+    count = Song.count
+    count.times do |i|
+      s=Song.find(i+1)
+      if s.artist == song_params[:artist] && s.name == song_params[:name]
+        flash[:alert] = "Song is already in the playlist!"
+        isthere = 1
+        break
+      end
+    end
+    if isthere!=1
+      @song = Song.create(song_params)    
+    end
+    show
   end
-  private
 
+  private
   def song_params
     params.require(:song).permit(:user_id, :name, :artist)
   end
